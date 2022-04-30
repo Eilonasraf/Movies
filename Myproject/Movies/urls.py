9110ef545a65ff545562c40d35526f0906900386
Eilonasraf <eilonasraf@gmail.com>
2022-04-30 21:56:12 +0300

    updated urls file

--- Myproject/Movies/urls.py
+++ Myproject/Movies/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-urlpatterns = [path('Movies/', views.Movies, name='Movies'), ]
+urlpatterns = [path('Movies/', views.Movies, name='Movies'),
+               path('Movies/home', views.Movies, name='Movies'),]
 
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
